3c2337a3d73e6ae91ec9b0410e2785eba2c2f23a
ndrdul <devdrdul@protonmail.com>
2022-04-22 12:54:38 -0400

    grid working

--- src/App.js
+++ src/App.js
@@ -3,16 +3,11 @@ import './App.css';
 function App() {
   return (
 
-    <div id="grid">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
-      <div>6</div>
-      <div>7</div>
-      <div>8</div>
-      <div>9</div>
+    <div className="grid">
+      <div className="nav">nav</div>
+      <div className="graph">graph</div>
+      <div className="node">node</div>
+      <div className="footer">footer</div>
     </div>
 
   );
